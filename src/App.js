34c8b98b0179017472b4ad3f02f5681483c154d5
RMYanenko <yanenko.r.m@gmail.com>
2019-12-02 00:10:27 +0200

    AnswerList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz'
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, Vlad!!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Quiz />
+      </Layout>
+    )
+  }
 }
 
-export default App;
+export default App
