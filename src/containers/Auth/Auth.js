daf5e8e54925d50249433be07dc6eee3760a436c
RMYanenko <yanenko.r.m@gmail.com>
2019-12-16 00:27:18 +0200

    Validashion auth form part 1

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import classes from './Auth.module.scss';
-import Button from '../../components/UI/Button/Button'
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
+
+    state = {
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Введите корректный email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Пароль',
+                errorMessage: 'Введите корректный пароль',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
 
     loginHandler = () =>{
 
@@ -16,6 +46,29 @@ export default class Auth extends Component {
         event.preventDefault()
     }
 
+    onChangeHandler = (event, controlName) =>{
+        console.log(`${controlName}: `, event.target.value)
+    }
+
+    renderInput() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                <Input
+                    key={controlName + index}
+                    type={control.type}
+                    value={control.value}
+                    valid={control.valid}
+                    touched={control.touched}
+                    label={control.label}
+                    shouldValidate={!!!control.validation}
+                    errorMessage={control.errorMessage}
+                    onChange={event => this.onChangeHandler(event, controlName)}
+                />
+            )
+        })
+    }
+
     render() {
     return (
         <div className={classes.Auth}>
@@ -23,10 +76,11 @@ export default class Auth extends Component {
                 <h1>Auth</h1>
 
                 <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-                    <input type="text"/>
-                    <input type="text"/>
+                   {
+                       this.renderInput()
+                   }
 
-                    <Button type="succec" onClick={this.loginHandler}>Войти</Button>
+                    <Button type="success" onClick={this.loginHandler}>Войти</Button>
                     <Button type="primary" onClick={this.registerHandler}>Зарегистирироватся</Button>
                 </form>
             </div>            
